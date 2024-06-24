75afcd2cfa1c03b1abc60f3d43e85b0cc143faef
JohnnyDeng6 <jzydeng@gmail.com>
2024-06-23 23:31:22 -0700

    init

---
+++ src/main/java/ca/cmpt276/examharmony/ExamHarmonyApplication.java
@@ -0,0 +1,13 @@
+package ca.cmpt276.examharmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamHarmonyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamHarmonyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/cmpt276/examharmony/ExamHarmonyApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.cmpt276.examharmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamHarmonyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
