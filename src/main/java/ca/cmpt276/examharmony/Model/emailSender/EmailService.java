6684d3d753daccb857123c0a81f087771be3a7ae
JohnnyDeng6 <jzydeng@gmail.com>
2024-07-09 02:04:51 -0700

    password reset revamp

--- src/main/java/ca/cmpt276/examharmony/Model/emailSender/EmailService.java
+++ src/main/java/ca/cmpt276/examharmony/Model/emailSender/EmailService.java
@@ -3,7 +3,6 @@ package ca.cmpt276.examharmony.Model.emailSender;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -13,16 +12,6 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
-
-    public void sendSimpleEmail(String toEmail, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(subject);
-        message.setText(body);
-        message.setFrom("examharmony6@gmail.com");
-
-        mailSender.send(message);
-    }
 
     public void sendHtmlEmail(String to, String subject, String body) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
