5592e390f2245c8618023698e31386c58770917b
JamesChuong <jcjameschuong@gmail.com>
2024-07-16 18:51:54 -0700

    Add beans to test file

--- src/test/java/ca/cmpt276/examharmony/Controllers/InstructorControllerTest.java
+++ src/test/java/ca/cmpt276/examharmony/Controllers/InstructorControllerTest.java
@@ -1,14 +1,28 @@
 package ca.cmpt276.examharmony.Controllers;
 
+import ca.cmpt276.examharmony.Model.user.User;
+import ca.cmpt276.examharmony.Model.user.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class InstructorControllerTest {
 
-    @Test
-    void instructorInfo() {
-    }
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private InstructorController instructorController;
+
+    @Autowired
+    private UserRepository userRepo;
+
 
     @Test
     void submitData() {
