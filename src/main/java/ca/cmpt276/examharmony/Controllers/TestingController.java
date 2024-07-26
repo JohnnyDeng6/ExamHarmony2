1ab77ba908d6ab79ea6785cb0867547efef2a8e5
JamesChuong <jcjameschuong@gmail.com>
2024-07-26 14:16:08 -0700

    Add test controller for adding editInterval to db

--- src/main/java/ca/cmpt276/examharmony/Controllers/TestingController.java
+++ src/main/java/ca/cmpt276/examharmony/Controllers/TestingController.java
@@ -2,6 +2,8 @@ package ca.cmpt276.examharmony.Controllers;
 
 import ca.cmpt276.examharmony.Model.CourseSectionInfo.CourseRepository;
 import ca.cmpt276.examharmony.Model.CourseSectionInfo.CoursesSec;
+import ca.cmpt276.examharmony.Model.EditInterval.EditInterval;
+import ca.cmpt276.examharmony.Model.EditInterval.IntervalRepository;
 import ca.cmpt276.examharmony.Model.roles.Role;
 import ca.cmpt276.examharmony.Model.roles.RoleRepository;
 import ca.cmpt276.examharmony.Model.user.User;
@@ -28,6 +30,9 @@ public class TestingController {
 
     @Autowired
     private PasswordEncoder pwEncorder;
+
+    @Autowired
+    private IntervalRepository intervalRepository;
 
     @GetMapping("/adminExamSlot")
     public String showAdminExamSlotPage() {
@@ -71,4 +76,13 @@ public class TestingController {
         newUser.setRoles(roleSet);
         userRepo.save(newUser);
     }
+
+    @GetMapping("/test/add/interval")
+    public void addEditInterval(){
+        EditInterval test = new EditInterval();
+        test.setStartTime("2024-06-15 13:00");
+        test.setEndTime("2028-06-15 13:00");
+        intervalRepository.save(test);
+    }
+
 }
