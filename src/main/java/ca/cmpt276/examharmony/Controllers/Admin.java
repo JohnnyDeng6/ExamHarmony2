77247fae2bc90f7fd6d3058e76360b54558dcf55
JohnnyDeng6 <jzydeng@gmail.com>
2024-07-28 19:56:10 -0700

    made send bcc work

--- src/main/java/ca/cmpt276/examharmony/Controllers/Admin.java
+++ src/main/java/ca/cmpt276/examharmony/Controllers/Admin.java
@@ -3,6 +3,7 @@ package ca.cmpt276.examharmony.Controllers;
 import ca.cmpt276.examharmony.Model.EditInterval.EditInterval;
 import ca.cmpt276.examharmony.Model.EditInterval.IntervalRepository;
 import ca.cmpt276.examharmony.Model.EditInterval.EditIntervalDTO;
+import ca.cmpt276.examharmony.Model.emailSender.EmailService;
 import ca.cmpt276.examharmony.Model.examRequest.ExamSlotRequest;
 import ca.cmpt276.examharmony.Model.examRequest.ExamSlotRequestRepository;
 import ca.cmpt276.examharmony.Model.user.User;
@@ -10,6 +11,7 @@ import ca.cmpt276.examharmony.Model.user.UserRepository;
 
 import ca.cmpt276.examharmony.utils.CustomUserDetails;
 import ca.cmpt276.examharmony.utils.InstructorExamSlotRepository;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,10 +19,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
 import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import ca.cmpt276.examharmony.Model.roles.RoleRepository;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
@@ -40,6 +46,9 @@ public class Admin {
 
     @Autowired
     private  InstructorExamSlotRepository instructorExamRepo;
+
+    @Autowired
+    private EmailService emailService;
 
     @GetMapping("/viewRequests")
     public String viewRequests(Model model) {
@@ -106,16 +115,43 @@ public class Admin {
     }
 
     @PostMapping("/emailAll")
-    public String emailAll(Model model){
-        List<User> users = userRepository.findAll();
+    public String emailAll(Model model, RedirectAttributes redirectAttributes) throws MessagingException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
             User admin = userRepository.findByUsername(userDetails.getUsername());
             model.addAttribute("admin", admin);
+
+            List<String> allEmails = userRepository.findAllEmailAddresses();
+            String[] to = allEmails.toArray(new String[0]);
+
+            String subject = "Editing period date set";
+            EditInterval interval = intervalRepository.findById(1);
+            String body = buildEditingPeriodEmailBody(interval.getStartTime(), interval.getEndTime());
+
+            emailService.sendEmailWithBCC(to, subject, body);
         }
+        redirectAttributes.addFlashAttribute("alertMessage", "Failed to send mass email, please try again in 24 hours");
         return "adminHome";
 
     }
+
+    private String buildEditingPeriodEmailBody(LocalDateTime startDate, LocalDateTime endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String formattedStartDate = startDate.format(formatter);
+        String formattedEndDate = endDate.format(formatter);
+
+        return "<p>Dear all users,</p>"
+                + "<p>This is an automated message sent to all Invigilators and Instructors.</p>"
+                + "<p>The editing period has been set/updated by administration.</p>"
+                + "<p>You may start editing within the set period from <strong>" + formattedStartDate + "</strong> to <strong>" + formattedEndDate + "</strong>:</p>"
+                + "<p>If you are an Invigilator, you will be able to set your availability and accept/reject requests within the set period. "
+                + "If you are an Instructor, you will be able to request your desired exam slot preference within the set period.</p>"
+                + "<p>If you are not a user of ExamHarmony, please ignore this email.</p>"
+                + "<p>Best regards,</p>"
+                + "<p>The ExamHarmony Team</p>"
+                + "<p><em>Note: This is an automated message, please do not reply.</em></p>";
+    }
+
 }
 
 
